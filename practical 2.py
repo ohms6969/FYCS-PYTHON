f131b56883c531f37e0ade5640e0af383d5272d6
ohms6969 <sohamumeshhoskar@gmail.com>
2025-07-23 16:35:20 +0530

    Add files via upload

---
+++ practical 2.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+c = 15
+
+if a<b and a<c:
+    print ("a is smallest")
+if b<a and b<c:
+    print ("a is smallest")
+else:
+    print ("c is smallest")
---
+++ practical 3.py
@@ -0,0 +1,13 @@
+for i in range (1,11):
+    print(2*i)
+
+A = 25
+B = 15
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+print(A//B)
+print(A**B)
